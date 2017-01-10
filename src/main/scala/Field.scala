1418d55cfec730fa754eb40a64e044d106248dd1
skht777 <skht@skhtlab.com>
2017-01-11 02:28:28 +0900

    リファクタリング：気に入らないところを簡略化した

--- src/main/scala/Field.scala
+++ src/main/scala/Field.scala
@@ -5,41 +5,42 @@ import Kind.T
   * @author skht777
   *
   */
-class Field(size: Point[Int]) {
-  def newState(blocks: Seq[Square] = Seq()): GameState = {
-    val point = Block(T, Point(size.x / 2.0, size.y))
-    GameState(blocks ++ point.current, point)
+case class Field(size: Point[Int]) {
+  def newState(blocks: Seq[Square] = Seq()): State = {
+    val dropPos = Point(size.x / 2.0, size.y)
+    State(Block(T, dropPos), blocks).load()
   }
 
-  val moveLeft = transit { _.moveBy(-1.0, 0.0) }
-  val moveRight = transit { _.moveBy(1.0, 0.0) }
-  val moveDown = transit { _.moveBy(0.0, -1.0) }
-  val rotateCW = transit { _.rotateBy(-math.Pi / 2.0) }
+  val moveLeft = transit(_.moveBy(-1.0, 0.0))
+  val moveRight = transit(_.moveBy(1.0, 0.0))
+  val rotateCW = transit(_.rotateBy(-math.Pi / 2.0))
+  val moveDown = transit(_.moveBy(0.0, -1.0))
 
-  private[this] def transit(trans: Block => Block,) = (s: GameState) =>
-    validate(GameState(unload(s.current, s.blocks), trans(s.current))) map (x =>
-      x.copy(blocks = load(x.current, x.blocks))) getOrElse s
+  private[this] def transit(trans: Block => Block) =
+    (s: State) => validate(s.unload()
+      .copy(current = trans(s.current))) map (_.load()) getOrElse s
 
-  private[this] def validate(s: GameState): Option[GameState] = {
+  private[this] def validate(s: State): Option[State] = {
     def inBounds(pos: Point[Int]): Boolean =
       pos.x >= 0 && pos.x < size.x && pos.y >= 0 && pos.y <= size.y + 2
 
-    val currentPoss = s.current.current map { _.pos }
-    if ((currentPoss forall inBounds) &&
-      (s.blocks map { _.pos } intersect currentPoss).isEmpty) Some(s)
+    if ((s.currentPos forall inBounds) &&
+      (s.blocsPos intersect s.currentPos).isEmpty) Some(s)
     else None
   }
-
-  private[this] def unload(p: Block, bs: Seq[Square]) = {
-    val currentPoss = p.current map { _.pos }
-    bs filterNot { currentPoss contains _.pos }
-  }
-
-  private[this] def load(p: Block, bs: Seq[Square]) = bs ++ p.current
 }
 
-case class GameView(blocks: Seq[Square], current: Block)
+case class View(current: Block, blocks: Seq[Square])
 
-case class GameState(blocks: Seq[Square], current: Block) {
-  def view: GameView = GameView(blocks, current)
+case class State(current: Block, blocks: Seq[Square]) {
+  def view: View = View(current, blocks)
+
+  def currentPos = current.current map { _.pos }
+
+  def blocsPos = blocks map { _.pos }
+
+  def load(b: Block = current) = copy(blocks = blocks ++ b.current)
+
+  def unload(b: Block = current) = copy(blocks =
+    blocks filterNot { b.current map { _.pos } contains _.pos })
 }
