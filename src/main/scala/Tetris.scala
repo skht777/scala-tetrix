755b698b7ebcb412db893b6a6be4ed9a4aac2f77
skht777 <skht@skhtlab.com>
2017-05-22 21:27:22 +0900

    描画系を簡潔にした

--- src/main/scala/Tetris.scala
+++ src/main/scala/Tetris.scala
@@ -28,28 +28,30 @@ object Tetris extends JFXApp {
 class fieldController(private val canvas: Canvas) {
   private[this] val unit = ViewUnit(Point(10, 20))
   private[this] val gc: GraphicsContext = jfxGraphicsContext2sfx(canvas getGraphicsContext2D)
-  private[this] val tl = Timeline(Seq(KeyFrame(Duration(1000),
-    onFinished = _ => transAndDraw(unit.down()))))
+  private[this] val tl = Timeline(Seq(KeyFrame(Duration(1000), onFinished = _ => transAndDraw(unit.down()))))
 
   tl.cycleCount = Timeline.Indefinite
+  tl.play()
   drawView()
 
-  private[this] def drawBlock(posX: Int, posY: Int, size: Int, exX: Int = 0, exY: Int = 0) = {
-    val (x, y) = (exX + posX * size, exY + posY * size)
+  private[this] def drawBlock(posX: Double, posY: Double, size: Int, exX: Double = 0, exY: Double = 0) = {
+    val p = Point(exX, exY) + Point(posX, posY) * size
     gc.fill = Color.White
     gc.stroke = Color.Black
-    gc.fillRect(x, y, size, size)
-    gc.strokeLine(x, y, x + size, y)
-    gc.strokeLine(x, y, x, y + size)
-    gc.strokeLine(x, y + size, x + size, y + size)
-    gc.strokeLine(x + size, y, x + size, y + size)
+    gc.fillRect(p.x, p.y, size, size)
+    strokeLine(p.x, p.y, size, 0)
+    strokeLine(p.x, p.y, 0, size)
+    strokeLine(p.x, p.y + size, size, 0)
+    strokeLine(p.x + size, p.y, 0, size)
   }
 
-  private[this] def drawLine(w: Int, h: Int, size: Int, exX: Int = 0, exY: Int = 0) = {
+  private[this] def strokeLine(x: Double, y: Double, w: Double, h: Double) = gc.strokeLine(x, y, x + w, y + h)
+
+  private[this] def strokeLine(w: Int, h: Int, size: Int, exX: Double = 0, exY: Double = 0) = {
     gc.fill = Color.Black
     gc.stroke = Color.White
-    0 to w foreach (i => gc.strokeLine(exX + size * i, exY, exX + size * i, exY + size * h))
-    0 to h foreach (i => gc.strokeLine(exX, exY + size * i, exX + size * w, exY + size * i))
+    0 to w map (i => i * size) foreach (x => strokeLine(exX + x, exY, 0, size * h))
+    0 to h map (i => i * size) foreach (y => strokeLine(exX, exY + y, size * w, 0))
   }
 
   private[this] def drawView() = {
@@ -59,11 +61,11 @@ class fieldController(private val canvas: Canvas) {
     val nextPos = Point(unit.size.x + 1, 2) * blockSize
     gc.fill = Color.Black
     gc.fillRect(0, 0, canvas getWidth, canvas getHeight)
-    drawLine(unit.size.x, unit.size.y, blockSize)
+    strokeLine(unit.size.x, unit.size.y, blockSize)
     unit.view.blocks foreach (b => drawBlock(b.pos.x, unit.size.y - b.pos.y - 1, blockSize))
 
     // draw next block
-    drawLine(4, 4, nextSize, nextPos.x, nextPos.y)
+    strokeLine(4, 4, nextSize, nextPos.x, nextPos.y)
     unit.view.next.copy(pos = Point(2.0, 2.0))
       .current foreach (b => drawBlock(b.pos.x, 4 - b.pos.y - 1, nextSize, nextPos.x, nextPos.y))
 
@@ -81,7 +83,6 @@ class fieldController(private val canvas: Canvas) {
   }
 
   def operate(key: KeyEvent): Unit = {
-    if (key.getCode == KeyCode.Enter.delegate) tl.play()
     val trans: State => State = jfxKeyCode2sfx(key.getCode) match {
       case KeyCode.Left => unit.left()
       case KeyCode.Right => unit.right()
